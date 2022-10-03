d928fbd8417b75ace2aa9af1d47158f12eaf56a3
ꜱʜɪʀᴀᴢ <44825811+TrendsetterShiraz@users.noreply.github.com>
2022-10-03 12:12:24 -0500

    Time: 182 ms (29.52%), Space: 14.1 MB (95.95%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,6 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         
-        nums = nums + nums
+        #One Liner Solution:
+        # return nums + nums #But this is cheating LOL...
         
-        return nums
+        # A 3 Liner Solution which shows you know your stuff.
+        for i in range(len(nums)): #Go Through each element of nums.
+            nums.append(nums[i]) #Append it to the existing nums.
+        return nums #return it.
