5c822167c364f5f07bf5e0fc3c92bc3b025cbc7d
ꜱʜɪʀᴀᴢ <44825811+TrendsetterShiraz@users.noreply.github.com>
2022-10-30 15:25:18 -0500

    Time: 110 ms (32.49%), Space: 16.5 MB (11.50%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,14 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #Game Plan for O(N)
+        
+        #Only grab the characters using Regular Expressions ad lowercase the s string
+        x = re.findall("[a-zA-Z0-9]", s.lower())
+        
+        #Stride the output from he negative index
+        res = x[::-1]
+        # and comapre
+        if str(x) == str(res):
+            return True
